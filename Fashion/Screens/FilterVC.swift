f4841e4817a0302364e602785eb7905219d14393
Ahmed Yehia <midoa9462@gmail.com>
2023-04-04 09:08:43 -0700

    Slider and rating

--- Fashion/Screens/FilterVC.swift
+++ Fashion/Screens/FilterVC.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import CheckMarkView
 
 class FilterVC: UIViewController {
     static let ID = String(describing: FilterVC.self)
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+  
 }
